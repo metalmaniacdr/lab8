745cc96481b115e549cf0b005cce5c61b4b5eb01
Dexter Rivas <drivas@cs.utsa.edu>
2011-11-17 17:29:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab8::Application.config.secret_token = '36b5020812c802780c5bd3f5083bb183186e24a31b7b7f42ec92dac4274ea5d8817558fe41b4410b0760ffea6c50b7037b68612de70aad4f7817bcd12987b0e0'
